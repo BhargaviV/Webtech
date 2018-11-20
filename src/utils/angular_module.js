f30c73b165ccc195d7b7df1272afe2af0843abfe
BhargaviV <bhargavivittalapuram@gmail.com>
2018-11-20 19:51:52 +0530

    Modified files

--- src/utils/angular_module.js
+++ src/utils/angular_module.js
@@ -1,12 +1,34 @@
 var app = angular.module("bookapp",[]);
 
-app.controller("ctrl",['$scope','$http',function($scope,$http)
+//factory to share objects between two controllers
+app.factory("factory",function()
+{
+    var cart = [];
+    function addtocart (book)
+    {
+        cart.push(book);
+        cart[cart.length-1].count = 1;
+        console.log(cart);
+    }
+    function getcartitems()
+    {
+        return cart;
+    }
+    return{
+        addtocart : addtocart,
+        getcartitems : getcartitems
+    }
+})
+
+//controller for index page
+app.controller("ctrl",['$scope','$http','$location','factory',function($scope,$http,$location, factory)
 {
         $scope.categories=[];
         $scope.page_number=[];
         $scope.books = [];
         $scope._pages=[];
-        //$scope.numberOfpages = 0;
+        $scope.cart = [];
+        $scope.getpagenumber = Array;
       
         $http.get("/getCategories").then(function(response)
         {
@@ -35,6 +57,29 @@ app.controller("ctrl",['$scope','$http',function($scope,$http)
 
         $http.get("/");
 
+       
+
+        $scope.addtocart =function(book)
+        {
+            factory.addtocart(book);
+            $scope.cart = factory.getcartitems();
+            console.log("cart",$scope.cart);
+        }
+
+        $scope.view = function()
+        {
+         
+            $location.path('/cart.html');
+        }
+
+        /* $scope.addtocart = function(book)
+        {
+            console.log(book);
+            $scope.cart.push(book);
+            var len  = $scope.cart.length;
+            $scope.cart[len-1].count = 1;
+            console.log($scope.cart);
+        }*/
         /*$scope.getBookByCategory = function(category) {
             $http.get("/books/"+category).then(function(response)
             {
@@ -46,5 +91,36 @@ app.controller("ctrl",['$scope','$http',function($scope,$http)
                 console.log(err);
             });
         }*/
-        $scope.getpagenumber = Array;
-}])
+    
+}]);
+
+// second controller for cart page
+app.controller("cartctrl",['$scope','$http','$location','factory',function($scope,$http,$location, factory)
+{
+    $scope.cart =  factory.getcartitems();
+
+    $scope.increasebooks = function(index)
+    {
+        $scope.cart[index].count +=1;
+    }
+
+    $scope.decreasebooks = function(index)
+    {
+        $scope.cart[index].count -=1;
+    }
+    console.log($scope.cart);
+}]);
+
+
+/*app.config(function($stateProvider,$urlRouterProvider)
+{
+    $stateProvider
+    .state('getcart',{
+        url:'/getcart',
+        templateUrl: '../templates/cart.html'
+    })
+    .state('addcart',{
+        url:'/addcart',
+        templateUrl: '../templates/index.html'
+    })
+});*/
